4e165a7f5233477ac5c13d1a61a95982794a2c88
Zach Reizner <zachr@google.com>
2017-08-14 17:16:55 -0700

    CHROMIUM: virtwl: add virtwl driver
    TEST=emerge-tatl chromeos-kernel-4_4
    BUG=chromium:738638
    
    [dgreid - conflicts with Makefile and find_vqs function + clean
    checkpatch.pl run]
    
    Change-Id: I6e8e128a5548c915a9561938cbb066edc8c42747
    Reviewed-on: https://chromium-review.googlesource.com/567299
    Commit-Ready: Zach Reizner <zachr@chromium.org>
    Tested-by: Zach Reizner <zachr@chromium.org>
    Reviewed-by: Zach Reizner <zachr@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/780812
    Commit-Ready: Dylan Reid <dgreid@chromium.org>
    Tested-by: Dylan Reid <dgreid@chromium.org>
    
    Conflicts:
    	drivers/virtio/Kconfig
    
    [rebase419(groeck): Context conflicts; squash with fixup patch;
    	kernel API change: sys_close -> ksys_close]
    Signed-off-by: Guenter Roeck <groeck@chromium.org>
    [rebase54(groeck): Context conflicts; squashed patches:
    	CHROMIUM: virtwl: fix incorrect list_for_each_entry_safe usage
    	CHROMIUM: virtwl: fix deadlock while receiving VFDs
    	CHROMIUM: virtwl: change VIRTWL_SEND_MAX_ALLOCS to match libwayland
    	CHROMIUM: virtwl: add compat_ioctl entry for each struct file_operations
    	CHROMIUM: virtwl: use compat_ptr in compat_ioctl implementations
    	CHROMIUM: virtwl: remove unneeded access_checks in ioctls
    	CHROMIUM: virtwl: check for CONFIG_COMPAT before defining compat ioctl
    	CHROMIUM: virtwl: fix asterisk placement around __user
    	CHROMIUM: virtwl: properly free qentries that are empty
    	CHROMIUM: virtwl: add pipe vfd support
    	CHROMIUM: virtwl: add dmabuf vfd support
    	CHROMIUM: virtwl: fix double free in error path for virtwl_ioctl_recv
    	CHROMIUM: virtwl: add dmabuf sync ioctl
    	CHROMIUM: virtio/wl: Fix locking order in do_new()
    	CHROMIUM: virtio/wl: Fix a missing mutex unlock in error path
    	CHROMIUM: virtio/wl: Unlock vfd->lock before freeing it
    	CHROMIUM: virtio/wl: Always update *vfd_count on recv, if given
    	CHROMIUM: virtwl: send foreign id support
    	CHROMIUM: virtwl: fix dma_buf_get() retval handling
    	CHROMIUM: virtwl: Kick the in virtqueue in virtwl_vfd_remove()
    	CHROMIUM: virtwl: Fix possible deadlocks.
    	CHROMIUM: virtwl: fix descriptor sizes for close, dmabuf_sync
    	Revert "CHROMIUM: virtwl: Fix possible deadlocks."
    ]
    Signed-off-by: Guenter Roeck <groeck@chromium.org>

--- include/uapi/linux/virtio_ids.h
+++ include/uapi/linux/virtio_ids.h
@@ -48,5 +48,6 @@
 #define VIRTIO_ID_FS           26 /* virtio filesystem */
 #define VIRTIO_ID_PMEM         27 /* virtio pmem */
 #define VIRTIO_ID_MAC80211_HWSIM 29 /* virtio mac80211-hwsim */
+#define VIRTIO_ID_WL           30 /* virtio wayland */
 
 #endif /* _LINUX_VIRTIO_IDS_H */
