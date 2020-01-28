3362fd3ed0b237228908c1a8e2098b8bd3339188
Keiichi Watanabe <keiichiw@chromium.org>
2020-01-28 19:35:21 +0900

    CHROMIUM: virtio: Accept new IDs for ChromeOS-specific devices
    (Cherry-pick from CL:2024135)
    Stop using 30 and 31 for virtio-wl and virtio-tpm, as these values
    are reserved for virtio-video devices [1].
    Instead, we shift IDs for ChromeOS-specific devices to larger
    integers.
    
    As the first step, this patch makes virtio-{wl,tpm}
    drivers accept both old and new device IDs.
    
    [1]: https://github.com/oasis-tcs/virtio-spec/issues/67
    
    BUG=chromium:1031512
    TEST=/dev/wl0 exists on ARCVM with and without CL:2025127
    
    Change-Id: I8e0fa7efd53cd8eb933574ebec20bc3d78efcf74
    Signed-off-by: Keiichi Watanabe <keiichiw@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2026774
    Commit-Queue: Stephen Barber <smbarber@chromium.org>
    Reviewed-by: Stephen Barber <smbarber@chromium.org>

--- include/uapi/linux/virtio_ids.h
+++ include/uapi/linux/virtio_ids.h
@@ -48,6 +48,11 @@
 #define VIRTIO_ID_FS           26 /* virtio filesystem */
 #define VIRTIO_ID_PMEM         27 /* virtio pmem */
 #define VIRTIO_ID_MAC80211_HWSIM 29 /* virtio mac80211-hwsim */
-#define VIRTIO_ID_WL           30 /* virtio wayland */
+#define VIRTIO_ID_WL_LEGACY    30 /* virtio wayland */
+#define VIRTIO_ID_TPM_LEGACY   31 /* virtio wayland */
+
+/* Chrome OS-specific devices */
+#define VIRTIO_ID_WL           63 /* virtio wayland */
+#define VIRTIO_ID_TPM          62 /* virtio tpm */
 
 #endif /* _LINUX_VIRTIO_IDS_H */
